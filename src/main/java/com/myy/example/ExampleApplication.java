8db4dccd9eb899932884275aa936346fc802d1a1
zhouyx <zhouyx@mingyuanyun.com>
2020-01-11 17:03:22 +0800

    引入thymeleaf模板

--- src/main/java/com/myy/example/ExampleApplication.java
+++ src/main/java/com/myy/example/ExampleApplication.java
@@ -2,6 +2,8 @@ package com.myy.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @SpringBootApplication
 public class ExampleApplication {
@@ -10,4 +12,8 @@ public class ExampleApplication {
 		SpringApplication.run(ExampleApplication.class, args);
 	}
 
+//	@Bean
+	public InternalResourceViewResolver internalResourceViewResolver(){
+		return new InternalResourceViewResolver("templates","");
+	}
 }
--- src/main/java/com/myy/example/controller/ExampleController.java
+++ src/main/java/com/myy/example/controller/ExampleController.java
@@ -1,16 +1,24 @@
 package com.myy.example.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
 
 @Controller
 public class ExampleController {
 
     @RequestMapping(value = "/hello-world", method = RequestMethod.GET)
+    @ResponseBody
     public String helloWorld() {
         return "templates/hello.html";
+    }
+
+    @RequestMapping("/view")
+    public String view(Map<String, Object> paramMap) {
+        paramMap.put("name", "zhangSan");
+        paramMap.put("age", 28);
+        return "hello";
     }
 }
